13fbed283ea932b50c06503cf68d5153c2c8f943
Duy K. Bui <duy@buifamily.info>
2022-04-28 10:42:01 -0400

    build: add trunk

--- sh/brew.sh
+++ sh/brew.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env sh
 
 if ! which brew; then
-  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
 else
-  brew update
+    brew update
 fi
 if test -f /home/linuxbrew/.linuxbrew/bin/brew; then
-  echo "/home/linuxbrew/.linuxbrew/bin/brew shellenv | source; or true" >~/.config/fish/conf.d/brew.fish
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  export HOMEBREW_PREFIX
-  export HOMEBREW_CELLAR
-  export HOMEBREW_REPOSITORY
-  export PATH
-  export MANPATH
-  export INFOPATH
+    echo "/home/linuxbrew/.linuxbrew/bin/brew shellenv | source; or true" >~/.config/fish/conf.d/brew.fish
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    export HOMEBREW_PREFIX
+    export HOMEBREW_CELLAR
+    export HOMEBREW_REPOSITORY
+    export PATH
+    export MANPATH
+    export INFOPATH
 fi
--- sh/sudo.sh
+++ sh/sudo.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 
 if [ ! -e /etc/sudoers.d/$USER ]; then
-  psw=$(whiptail --passwordbox "Enter your sudo password to continue." 8 64 3>&1 1>&2 2>&3)
-  echo $psw | sudo -S mkdir -p /etc/sudoers.d
-  echo "$USER ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER > /dev/null
+    psw=$(whiptail --passwordbox "Enter your sudo password to continue." 8 64 3>&1 1>&2 2>&3)
+    echo $psw | sudo -S mkdir -p /etc/sudoers.d
+    echo "$USER ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER >/dev/null
 fi
