19fd3cd65585bac42f4afc03087f50b200853a93
Duy K. Bui <duy@buifamily.info>
2022-04-27 23:38:21 -0400

    feat: better init

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 
 test -d ~/dotfiles || git clone https://github.com/khuongduybui/dotfiles.git/ ~/dotfiles
 cd ~/dotfiles
-git pull --no-rebase -f
+git fetch
+git reset --hard origin/main
 git submodule update --init --recursive
 rm -f ~/dotfiles/install
 cp dotbot/tools/git-submodule/install ~/dotfiles/install
