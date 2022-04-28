9a8ce8a3ccfefebbc611ce38c1f88bf9617ed576
Duy K. Bui <duy@buifamily.info>
2022-04-27 23:12:04 -0400

    feat: add atuin

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 test -d ~/dotfiles || git clone https://github.com/khuongduybui/dotfiles.git/ ~/dotfiles
 cd ~/dotfiles
+git pull --no-rebase -f
 git submodule update --init --recursive
 rm -f ~/dotfiles/install
 cp dotbot/tools/git-submodule/install ~/dotfiles/install
