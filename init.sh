8af71eafd8d2bd9b8196565ca1f24d3b643aeac1
Duy K. Bui <duy@buifamily.info>
2022-04-28 10:08:45 -0400

    feat: add brew

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ git reset --hard origin/main
 git submodule update --init --recursive
 rm -f ~/dotfiles/install
 cp dotbot/tools/git-submodule/install ~/dotfiles/install
-~/dotfiles/install
+~/dotfiles/install --plugin-dir dotbot-brew
