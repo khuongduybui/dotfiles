f87f88fc44a7a9ad116cc40a432557434730674b
Duy K. Bui <duy@buifamily.info>
2022-05-01 15:14:49 -0400

    feat: only install sft if missing

--- sh/sft.sh
+++ sh/sft.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env sh
 
-# https://www.scaleft.com/docs/sft-ubuntu/
-echo "deb http://pkg.scaleft.com/deb linux main" | sudo tee /etc/apt/sources.list.d/scaleft.list
-curl -C - https://dist.scaleft.com/pki/scaleft_deb_key.asc | sudo apt-key add -
-sudo apt update
-sudo apt install scaleft-client-tools
+if ! which sft >/dev/null 2>&1; then
+    # https://www.scaleft.com/docs/sft-ubuntu/
+    echo "deb http://pkg.scaleft.com/deb linux main" | sudo tee /etc/apt/sources.list.d/scaleft.list
+    curl -C - https://dist.scaleft.com/pki/scaleft_deb_key.asc | sudo apt-key add -
+    sudo apt -y update
+    sudo apt -y install scaleft-client-tools
+fi
