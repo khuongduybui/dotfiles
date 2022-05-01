815be0e100c0ee093b1cfe3870098e20ff2f30fe
Duy K. Bui <duy@buifamily.info>
2022-05-01 00:54:47 -0400

    fix: sh/asdf.sh no longer fatally fails

--- sh/asdf.sh
+++ sh/asdf.sh
@@ -2,17 +2,18 @@
 
 if [ ! -d ~/.asdf ]; then
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+    mkdir -p ~/.config/fish/completions
+    cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+else
+    asdf update
+    asdf plugin update --all
 fi
-# shellcheck disable=SC1090
-. ~/.asdf/asdf.sh
-mkdir -p ~/.config/fish/completions
-cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
-asdf update
-asdf plugin update --all
 
 sudo apt install -y pkg-config make build-essential
-sudo apt install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-sudo apt install -y librust-openssl-dev libssl-dev
+sudo apt install -y libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt install -y llvm || true
+sudo apt install -y tk-dev || true
+sudo apt install -y librust-openssl-dev || sudo apt install -y libssl-dev
 
 # if [ -d /home/linuxbrew/.linuxbrew/bin ]; then
 #     mv /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/bin.bak
