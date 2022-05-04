d9b98104487c51020ae31545c9afe79869d383a0
Duy K. Bui <duy@buifamily.info>
2022-05-04 14:48:02 -0400

    feat: add apt

--- init.sh
+++ init.sh
@@ -8,3 +8,4 @@ git submodule update --init --recursive
 rm -f ~/dotfiles/install
 cp dotbot/tools/git-submodule/install ~/dotfiles/install
 ~/dotfiles/install --plugin-dir dotbot-brew --plugin-dir dotbot-asdf
+sudo ~/dotfiles/install -p dotbot-apt/apt.py -c ~/dotfiles/packages.conf.yaml
