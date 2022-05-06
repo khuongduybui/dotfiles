7fd58ed636994baf50a1e0c2b2da6cf32549fc3b
Duy K. Bui <duy@buifamily.info>
2022-05-05 22:09:48 -0400

    feat: different backup-code way

--- .config/fish/functions/backup-code.fish
+++ .config/fish/functions/backup-code.fish
@@ -1,15 +1,6 @@
-# Defined in /tmp/fish.SXtqg0/backup-code.fish @ line 2
 function backup-code
+    mkdir -p ~/winhome/Pengwin/backups
     cd ~/code
-    ls -1 ./*.code-workspace >.auto-backup
-    find -name .env >>.auto-backup
-    find -name .envrc >>.auto-backup
-    find -name .tool-versions >>.auto-backup
-    find -name 'local-tests*' >>.auto-backup
-    mkdir -p ~/winhome/code/
-    rsync --files-from=.auto-backup ./ ~/winhome/.code/
-    if test -f .manual-backup
-        rsync --files-from=.manual-backup ./ ~/winhome/.code/
-    end
+    tar -czf ~/winhome/Pengwin/backups/code.tgz .
     cd -
 end
--- .config/fish/functions/ips.fish
+++ .config/fish/functions/ips.fish
@@ -1,12 +1,11 @@
-# Defined in /tmp/fish.guwUXW/ips.fish @ line 2
 function ips
     set -lx query $argv[1]
     test -z $query; and set -lx query ""
     set -lx AWS_PROFILE (cat ~/.aws/config | grep '\[profile' | sed -e 's/\[profile //' -e 's/\]//' | fzf -1 -q $query)
     test -z $AWS_PROFILE; and return
     if __is_mac
-        /usr/local/bin/fish
+        fish
     else
-        /usr/bin/fish
+        fish
     end
 end
