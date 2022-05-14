c69da20cd3b863ad714f2cb961e2e37fdcfed9a6
Duy K. Bui <duy@buifamily.info>
2022-05-13 21:43:26 -0400

    feat: better msi-init

--- .config/fish/functions/msi-init.fish
+++ .config/fish/functions/msi-init.fish
@@ -1,20 +1,17 @@
 function msi-init
-    set -l verbose off
-    if test (echo $argv[1]) = "-v"
-        set -x verbose on
-    end
-
     echo "====== AWS SSO login ======"
     aws-sso-util login --all
 
-    echo "==== ActiveEye Gateway ===="
-    for pod in hawk wasp lion orca wolf bear
-        if test (echo $verbose) = "on"
-            echo "https://gateway1.$pod.activeeye.com/gateway"
-        end
-        xdg-open "https://gateway1.$pod.activeeye.com/gateway"
-    end
-
     echo "========= ScaleFt ========="
     sft login
+
+    echo "==== ActiveEye Gateway ===="
+    for pod in hawk wasp lion orca wolf bear
+        if ssh -q gateway1.ec2.$pod.activeeye.com exit
+            echo "gateway1.ec2.$pod.activeeye.com is up"
+        else
+            echo "https://gateway1.$pod.activeeye.com/gateway"
+            xdg-open "https://gateway1.$pod.activeeye.com/gateway"
+        end
+    end
 end
