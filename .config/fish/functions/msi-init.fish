35031571994780f4a73b8faf7b32b8ac78df2e9d
Duy K. Bui <duy@buifamily.info>
2022-05-13 22:01:11 -0400

    feat: better msi-init

--- .config/fish/functions/msi-init.fish
+++ .config/fish/functions/msi-init.fish
@@ -1,11 +1,11 @@
 function msi-init
-    rich --panel rounded -c -p "[bold italic]AWS SSO"
+    rich -a rounded -S blue -p "[blue]AWS SSO"
     aws-sso-util login --all
 
-    rich --panel rounded -c -p "[bold italic]ScaleFt"
+    rich -a rounded -S blue -p "[blue]ScaleFt"
     sft login
 
-    rich --panel rounded -c -p "[bold italic]ActiveEye Gateway"
+    rich -a rounded -S blue -p "[blue]ActiveEye Gateway"
     for pod in hawk wasp lion orca wolf bear
         if ssh -o ConnectTimeout=5 -q gateway1.ec2.$pod.activeeye.com exit
             rich -p "[green][bold italic]gateway1.ec2.$pod.activeeye.com[/bold italic] is up"
