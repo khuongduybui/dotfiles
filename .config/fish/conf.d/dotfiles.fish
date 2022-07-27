ec226f75ddab65af852f60c4e67eb26b0d5e759a
Duy K. Bui <duy@buifamily.info>
2022-07-26 22:54:13 -0400

    feat: only show atuin in fish with Ctrl R

--- .config/fish/conf.d/dotfiles.fish
+++ .config/fish/conf.d/dotfiles.fish
@@ -87,4 +87,4 @@ type -q zoxide; and zoxide init fish | sed "s/complete -e z/complete -c z -e/" |
 
 type -q jt; and test -e ~/.config/jira; and jt completion fish | source
 
-type -q atuin; and atuin init fish | source
+type -q atuin; and atuin init fish | source; and set -Ux ATUIN_SUPPRESS_TUI true
