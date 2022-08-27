91c80f644d7ac577148103acce844da308ffe7ae
Duy K. Bui <duy@buifamily.info>
2022-08-27 15:20:29 -0400

    feat: add msi-bitbucket

--- .config/fish/functions/msi-init.fish
+++ .config/fish/functions/msi-init.fish
@@ -5,6 +5,11 @@ function msi-init
     rich -a rounded -S blue -p "[blue]ScaleFt"
     sft login
 
+    rich -a rounded -S blue -p "[blue]MSI BitBucket"
+    rich --style italic -p "From [blue]https://confluence.mot-solutions.com/display/REMOT/Bitbucket+Access+Flow[/blue] (last updated [bold]19-Aug-2022[/bold])."
+    rich --style italic -p "Saved to [blue]s3://duybui-msi-shellscripts/msi-bitbucket.sh[/blue] in [blue]aws.aec.dev.duy[/blue] account."
+    aws --profile=sandbox s3 cp s3://duybui-msi-shellscripts/msi-bitbucket.sh - | bash -
+
     rich -a rounded -S blue -p "[blue]ActiveEye Gateway"
     for pod in hawk wasp lion orca wolf bear
         if ssh -o ConnectTimeout=5 -q gateway1.ec2.$pod.activeeye.com exit
