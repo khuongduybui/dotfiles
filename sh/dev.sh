d0bfb856cada9957629b8c5d54afd0b9cfe90aa2
Duy K. Bui <duy@buifamily.info>
2022-09-12 14:58:09 -0400

    feat: use iterm2 shell integration only for WezTerm

--- sh/dev.sh
+++ sh/dev.sh
@@ -14,6 +14,7 @@ cargo install difftastic
 asdf reshim
 
 test -e ~/.iterm2_shell_integration.fish || curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
+sed -e 's|test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish|test -e {$HOME}/.iterm2_shell_integration.fish ; and string match -q "$TERM_PROGRAM" WezTerm ; and source {$HOME}/.iterm2_shell_integration.fish|' -i ~/.config/fish/config.fish
 
 gh extension install mislav/gh-branch
 gh extension install redraw/gh-install
