951675edb46d39702f0aef5c6e56dd53d37ceb2a
Duy K. Bui <duy@buifamily.info>
2022-09-18 16:45:09 -0400

    feat: idempotent sh/dev.sh

--- sh/dev.sh
+++ sh/dev.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 
-pip install -U pipx
+pip install -U pip pipx
 asdf reshim
 mkdir -p /home/duybui/.virtualenvs
 pipx install virtualfish
@@ -16,5 +16,6 @@ asdf reshim
 test -e ~/.iterm2_shell_integration.fish || curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
 sed -e 's|test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish|test -e {$HOME}/.iterm2_shell_integration.fish ; and string match -q "$TERM_PROGRAM" WezTerm ; and source {$HOME}/.iterm2_shell_integration.fish|' -i ~/.config/fish/config.fish
 
-gh extension install mislav/gh-branch
-gh extension install redraw/gh-install
+gh extension list | grep -q mislav/gh-branch || gh extension install mislav/gh-branch
+gh extension list | grep -q redraw/gh-install || gh extension install redraw/gh-install
+gh extension list | grep -q gennaro-tedesco/gh-f || gh extension install gennaro-tedesco/gh-f
