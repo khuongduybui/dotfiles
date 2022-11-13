aad26061b6bcbe1e47754de4deb22fe2578b2c6e
Duy K. Bui <duy@buifamily.info>
2022-11-13 12:57:32 -0500

    feat: add asdf-cleanup.sh

---
+++ sh/asdf-cleanup.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env sh
+
+# echo "Finding all versions in use"
+# fd --hidden .tool-versions | xargs cat | sort | uniq
+
+for plugin in $(asdf plugin list); do
+    for version in $(asdf list "${plugin}" | xargs); do
+        echo "Checking ${plugin} ${version}"
+        if fd --hidden .tool-versions | xargs cat | grep -q "${plugin} ${version}"; then
+            echo "✅ in use"
+        else
+            echo "🗑️ removing"
+            asdf uninstall "${plugin}" "${version}"
+        fi
+    done
+done
