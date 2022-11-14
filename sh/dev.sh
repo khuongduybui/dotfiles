e9cd9bffeb4925ecfaddd1aec756322a6faf1d7f
Duy K. Bui <duy@buifamily.info>
2022-11-13 21:42:17 -0500

    feat: always reinstall virtual fish

--- sh/dev.sh
+++ sh/dev.sh
@@ -3,7 +3,7 @@
 pip install -U pip pipx
 asdf reshim
 mkdir -p /home/duybui/.virtualenvs
-pipx install virtualfish
+pipx install --force virtualfish
 pipx ensurepath
 fish -c "vf install"
 fish -c "vf addplugins auto_activation"
@@ -16,6 +16,7 @@ npm install -g dotenv-vault nativescript
 asdf reshim
 
 test -e ~/.iterm2_shell_integration.fish || curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
+# trunk-ignore(shellcheck/SC2016)
 sed -e 's|test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish|test -e {$HOME}/.iterm2_shell_integration.fish ; and string match -q "$TERM_PROGRAM" WezTerm ; and source {$HOME}/.iterm2_shell_integration.fish|' -i ~/.config/fish/config.fish
 
 gh extension list | grep -q mislav/gh-branch || gh extension install mislav/gh-branch
