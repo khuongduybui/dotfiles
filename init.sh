7f2e13af41f67dd50f1ea7a67ee9ab3466c4f19d
Duy K. Bui <duy@buifamily.info>
2022-12-24 00:03:25 -0500

    feat: solve brew once and for all

--- init.sh
+++ init.sh
@@ -7,4 +7,14 @@ git reset --hard origin/main
 git submodule update --init --recursive
 rm -f ~/dotfiles/install
 cp dotbot/tools/git-submodule/install ~/dotfiles/install
-~/dotfiles/install --plugin-dir dotbot-brew --plugin-dir dotbot-asdf
+
+if test -f /home/linuxbrew/.linuxbrew/bin/brew; then
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
+    echo "/home/linuxbrew/.linuxbrew/bin/brew shellenv | source; or true" >~/.config/fish/conf.d/brew.fish
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
+fi
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew update
+
+~/dotfiles/install --plugin-dir dotbot-asdf --plugin-dir dotbot-brew
+
