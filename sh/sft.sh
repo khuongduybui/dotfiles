4661a73805b04264b0751fed44b9e3777d79da1e
Duy K. Bui <duy@buifamily.info>
2023-03-04 23:03:16 -0500

    feat: update sft repo

--- sh/sft.sh
+++ sh/sft.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env sh
 
 if ! which sft >/dev/null 2>&1; then
-    # https://www.scaleft.com/docs/sft-ubuntu/
-    echo "deb http://pkg.scaleft.com/deb linux main" | sudo tee /etc/apt/sources.list.d/scaleft.list
-    curl -C - https://dist.scaleft.com/pki/scaleft_deb_key.asc | sudo apt-key add -
+    # https://help.okta.com/asa/en-us/Content/Topics/Adv_Server_Access/docs/sft-ubuntu.htm
+    curl -fsSL https://dist.scaleft.com/GPG-KEY-OktaPAM-2023 | gpg --dearmor | sudo tee /usr/share/keyrings/oktapam-2023-archive-keyring.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/oktapam-2023-archive-keyring.gpg] https://dist.scaleft.com/repos/deb jammy okta" | sudo tee /etc/apt/sources.list.d/scaleft.list
     sudo apt -y update
-    sudo apt -y install scaleft-client-tools
+    sudo apt -y install scaleft-client-tools scaleft-url-handler
 fi
