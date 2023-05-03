3c8fa9cbe8de1c75f94918c7c3e51c99e8c243bb
Duy K. Bui <duy@buifamily.info>
2023-05-02 23:28:35 -0400

    feat: use bash -l

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 test -d ~/dotfiles || git clone https://github.com/khuongduybui/dotfiles.git/ ~/dotfiles
 cd ~/dotfiles || exit
@@ -8,8 +8,8 @@ git submodule update --init --recursive
 rm -f ~/dotfiles/install
 cp dotbot/tools/git-submodule/install ~/dotfiles/install
 
-SHELL=bash ~/dotfiles/install --config-file prerequisites.conf.yaml
+bash -l -c '~/dotfiles/install --config-file prerequisites.conf.yaml'
 test -f /home/linuxbrew/.linuxbrew/bin/brew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 test -f ~/.asdf/asdf.sh && . ~/.asdf/asdf.sh
 sudo apt-get install -y unzip xdg-utils wslu
-SHELL=bash ~/dotfiles/install --plugin-dir dotbot-asdf --plugin-dir dotbot-brew
+bash -l -c '~/dotfiles/install --plugin-dir dotbot-asdf --plugin-dir dotbot-brew'
