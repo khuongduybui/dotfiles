fe2b20089852cfc6357ea5b85e0f45c76cd16c80
Duy K. Bui <duy@buifamily.info>
2024-03-03 15:23:56 -0500

    fix: remove --unstable from Deno tools

--- sh/shell-utils.sh
+++ sh/shell-utils.sh
@@ -2,7 +2,7 @@
 
 cd ~/dotfiles/shell-utils || exit
 for script in scripts/*.ts; do
-    deno install -f --no-check -A --unstable --reload "${HOME}/dotfiles/shell-utils/${script}"
+    deno install -f --no-check -A --reload "${HOME}/dotfiles/shell-utils/${script}"
 done
 if which deno | grep -q asdf; then
     asdf reshim
