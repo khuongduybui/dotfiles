ffb3b58c59e9ffa85a4bc3b7589695b5a840bd55
Duy K. Bui <duy@buifamily.info>
2024-03-03 15:28:32 -0500

    chore: lint

--- sh/shell-utils.sh
+++ sh/shell-utils.sh
@@ -4,6 +4,6 @@ cd ~/dotfiles/shell-utils || exit
 for script in scripts/*.ts; do
     deno install -f --no-check -A --reload "${HOME}/dotfiles/shell-utils/${script}"
 done
-if which deno | grep -q asdf; then
+if command -v deno | grep -q asdf; then
     asdf reshim
 fi
