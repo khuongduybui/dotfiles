50bb2b1ab141c0c70cfb9c2025003d86b18323a5
Duy K. Bui <duy@buifamily.info>
2024-03-24 14:22:01 -0400

    feat: sh/bamboo.sh

---
+++ sh/bamboo.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+sudo add-apt-repository -y ppa:bamboo-engine/ibus-bamboo
+sudo apt-get update -y
+sudo apt-get install -y ibus ibus-bamboo --install-recommends
+ibus restart
+# Đặt ibus-bamboo làm bộ gõ mặc định
+env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
