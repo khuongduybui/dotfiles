195b346a86cabb7815aeff7ad27eb68e45b52879
Duy K. Bui <duy@buifamily.info>
2024-03-24 15:48:38 -0400

    feat: brew install granted

--- .config/fish/functions/ips.fish
+++ .config/fish/functions/ips.fish
@@ -3,9 +3,6 @@ function ips
     test -z $query; and set -lx query ""
     set -lx AWS_PROFILE (cat ~/.aws/config | grep '\[profile' | sed -e 's/\[profile //' -e 's/\]//' | fzf -1 -q $query)
     test -z $AWS_PROFILE; and return
-    if __is_mac
-        fish
-    else
-        fish
-    end
+    # assume $AWS_PROFILE --exec "fish -l"
+    fish -l
 end
