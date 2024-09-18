39b41d8b879c9f9b5feeb6d30213ae7c475705f8
Duy K. Bui <duy@buifamily.info>
2024-09-18 00:51:48 -0400

    feat(reminder): add granted

--- .config/fish/functions/rsync-ssh.fish
+++ .config/fish/functions/rsync-ssh.fish
@@ -14,6 +14,9 @@ function rsync-ssh
     test -d ~/winhome/Insync/duy@buifamily.info/OneDrive
     and bash ~/setup/backup-and-link.sh ~/winhome/Insync/duy@buifamily.info/OneDrive/Essentials/dotfile.ssh ~/.ssh
 
+    test -d ~/winhome/.ssh
+    and bash ~/setup/backup-and-link.sh ~/winhome/.ssh ~/.ssh
+
     chmod 600 ~/.ssh/*.private.asc
     chmod 600 ~/.ssh/*.pem
     chmod 600 ~/.ssh/config
