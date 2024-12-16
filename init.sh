45b9f5fbb0350e6ebf689e95e0b5e0e9195e62c7
Duy K. Bui <duy@buifamily.info>
2024-12-16 01:53:36 -0500

    feat: ignore pipx

--- init.sh
+++ init.sh
@@ -8,9 +8,14 @@ git submodule update --init --recursive
 rm -f ~/dotfiles/install
 cp dotbot/tools/git-submodule/install ~/dotfiles/install
 
+# trunk-ignore(shellcheck/SC2088)
 SHELL=bash bash -l -c '~/dotfiles/install --config-file prerequisites.conf.yaml'
+# trunk-ignore(shellcheck/SC2312)
 test -f /home/linuxbrew/.linuxbrew/bin/brew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# trunk-ignore(shellcheck/SC1090)
 test -f ~/.asdf/asdf.sh && . ~/.asdf/asdf.sh
 sudo apt-get install -y unzip xdg-utils
+# trunk-ignore(shellcheck/SC2015)
 grep -q -i "Microsoft" /proc/version && sudo apt-get install -y wslu || true
+# trunk-ignore(shellcheck/SC2088)
 SHELL=bash bash -l -c '~/dotfiles/install --plugin-dir dotbot-asdf --plugin-dir dotbot-brew'
