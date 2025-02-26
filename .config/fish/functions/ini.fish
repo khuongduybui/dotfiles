5cfb867aa072713f47b0071685fc8fccd25d13e5
Duy K. Bui <duy@buifamily.info>
2025-02-26 13:41:13 -0500

    feat(ini): make <key> optional

--- .config/fish/functions/ini.fish
+++ .config/fish/functions/ini.fish
@@ -2,6 +2,8 @@
 function ini
 	if test (count $argv) = 2
         sed -nr "/^\[$argv[1]\]/ { :l /^$argv[2] *=/ { s/.*= *//; p; q;}; n; b l;}"
+    else if test (count $argv) = 1
+        awk "/\[$argv[1]\]/,/\$^/"
     else
         echo "Usage: ini <section> <key>"
     end
