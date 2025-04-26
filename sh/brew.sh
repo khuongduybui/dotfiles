83ebdaef0e91f557db19e5e204b8bb89b2a64a99
Duy K. Bui <duy@buifamily.info>
2025-04-26 18:35:18 -0400

    chore: trunk fmt

--- sh/brew.sh
+++ sh/brew.sh
@@ -3,7 +3,7 @@
 if ! test -f /home/linuxbrew/.linuxbrew/bin/brew; then
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
     echo "/home/linuxbrew/.linuxbrew/bin/brew shellenv | source; or true" >~/.config/fish/conf.d/brew.fish
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.profile
 else
     /home/linuxbrew/.linuxbrew/bin/brew update
 fi
